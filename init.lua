51dcc001c790f66acf13e1545aee1243d783fffb
Pessi <pessiv@outlook.com>
2023-08-22 05:37:07 +0300

    added_func

--- init.lua
+++ init.lua
@@ -1,6 +1,23 @@
 print("You feelings are valid and you are valued.")
 vim.cmd.colorscheme('torte')
-vim.keymap.set('n', 'äwhore', function() print("press me harder daddy :3") end)
+-- These are supposed to be my custom functions
+-- extra kinky shit func ()
+vim.g.kinky_number = 1
+function extrakinky ()
+	if vim.g.kinky_number == 1 then
+		print("press me harder mommy :3")
+		vim.g.kinky_number = vim.g.kinky_number + 1
+	elseif vim.g.kinky_number == 2 then
+		print("oh yes harder!!!!!! <3")
+		vim.g.kinky_number = vim.g.kinky_number + 1
+	elseif vim.g.kinky_number == 3 then
+		print("AHHHHHHHHHHHHHH <3 <3 <3 <3")
+		vim.g.kinky_number = 1
+	end
+end
+-- these are supposed to be my own custom bindings that make sense to me.
+-- I'm telling this so that you know why this entire thing is so shit
+vim.keymap.set('n', 'äwhore', function() extrakinky() end)
 vim.keymap.set('n', 'j','gj')
 vim.keymap.set('n', 'k','gk')
 vim.keymap.set('n', 'äc', ':!gcc -Wall -Wextra -Werror -Wpedantic *.c<Return>')
