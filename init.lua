56c4129fc488c8505d2da02bbba36b24a69a2b12
Pessi <pessiv@outlook.com>
2023-08-29 22:58:55 +0300

    added_better_keys

--- init.lua
+++ init.lua
@@ -15,7 +15,16 @@ function extrakinky ()
 		vim.g.kinky_number = 1
 	end
 end
-
+-- get the correct backtracking keymapping for the shit that you are doing
+function getcorrectkeys ()
+	if vim.bo.filetype == "c" then
+		vim.keymap.set('n','<F12>','<C-T>')
+		print("wake me up")
+	else
+		vim.keymap.set('n','<F12>','<C-O>')
+		print("before you gogo")
+	end
+end
 -- these are supposed to be my own custom bindings that make sense to me.
 -- I'm telling this so that you know why this entire thing is so shit
 vim.keymap.set('n', 'äwhore', function() extrakinky() end)
@@ -26,4 +35,4 @@ vim.keymap.set('n', 'ätc', ':tabnew<Return>')
 vim.keymap.set('n', 'ätn', ':tabnext<Return>')
 vim.keymap.set('n', 'ätp', ':tabprevious<Return>')
 vim.keymap.set('n', '<F11>', '<C-]>')
-vim.keymap.set('n', '<F12>', '<C-O>')
+vim.keymap.set('n', '<F12>', function() getcorrectkeys() end)
